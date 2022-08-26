1cf8f76ee0176411f8d91dad58dca96badfb9de8
VinayDasari07 <vinay.dasari.07@gmail.com>
2022-08-27 00:50:04 +0530

    Completed Header of Share Modal

---
+++ components/share-footer.component.tsx
@@ -0,0 +1,11 @@
+import { Typography, Box } from "@mui/material"
+
+const ShareFooter = () => {
+    return (
+        <Box>
+            <Typography>learn about sharing</Typography>
+        </Box>
+    )
+}
+
+export default ShareFooter
--- components/share-modal.component.tsx
+++ components/share-modal.component.tsx
@@ -1,4 +1,5 @@
-import { Box, useTheme } from "@mui/material"
+import { Box, Divider, useTheme } from "@mui/material"
+import ShareFooter from "./share-footer.component"
 import ShareHeader from "./share-header.component"
 
 const ShareModal = () => {
@@ -8,9 +9,12 @@ const ShareModal = () => {
             // position: "relative",
             width: theme.spacing(64),
             height: theme.spacing(32.75),
-            border: 1
+            opacity: 1,
+            boxShadow: 3
         }}>
             <ShareHeader />
+            <Divider />
+            <ShareFooter />
         </Box> 
     )
 }
